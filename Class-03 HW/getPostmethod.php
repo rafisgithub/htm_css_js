26bc02cbe7b3bbf9f16c1a418ab8dae7a9b2497e
Rafi Ahmed <84710018+Rafi120-max@users.noreply.github.com>
2022-09-08 23:27:48 +0600

    Add files via upload

---
+++ Class-03 HW/getPostmethod.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="getPostmethod.php" method="get">
+    username : <input type="text" name="username"><br><br>
+    password : <input type="password" name="pass"><br><br>
+    <input type="submit" value="Log in">
+</form>
+</body>
+</html>
